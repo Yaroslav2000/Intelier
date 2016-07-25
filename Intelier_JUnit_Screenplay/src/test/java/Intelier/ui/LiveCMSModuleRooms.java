1fd80526f8cff27897a2f6031396d10be4f63527
Yaroslav <yaroslav2000@hotmail.com>
2016-07-24 22:52:44 -0400

    Initial check-in

---
+++ Intelier_JUnit_Screenplay/src/test/java/Intelier/ui/LiveCMSModuleRooms.java
@@ -0,0 +1,10 @@
+package Intelier.ui;
+
+import org.openqa.selenium.By;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.targets.Target;
+import net.thucydides.core.annotations.DefaultUrl;
+
+@DefaultUrl("http://www.google.co.uk")
+public class LiveCMSModuleRooms extends PageObject {}
