3ac6380d219838b8bff14f7530c9d739e9759b06
Yaroslav <yaroslav2000@hotmail.com>
2016-07-24 22:58:22 -0400

    Archetypes check-in

---
+++ Archetype_Screenplay_Selenium_JUnit_artifactId/src/test/java/Archetype_Screenplay_Selenium_JUnit_package/tasks/OpenTheApplication.java
@@ -0,0 +1,19 @@
+package Archetype_Screenplay_Selenium_JUnit_package.tasks;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Open;
+import net.thucydides.core.annotations.Step;
+import Archetype_Screenplay_Selenium_JUnit_package.ui.GoogleSearchPage;
+
+public class OpenTheApplication implements Task {
+
+    GoogleSearchPage googleSearchPage;
+
+    @Step("Open the application")
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Open.browserOn().the(googleSearchPage)
+        );
+    }
+}
---
+++ Archetype_Selenium_JUnit_Cucumber_artifactId/src/test/java/Archetype_Selenium_JUnit_Cucumber_package/DefinitionTestSuite.java
@@ -0,0 +1,9 @@
+package Archetype_Selenium_JUnit_Cucumber_package;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="src/test/resources/features/consult_dictionary/LookupADefinition.feature")
+public class DefinitionTestSuite {}
