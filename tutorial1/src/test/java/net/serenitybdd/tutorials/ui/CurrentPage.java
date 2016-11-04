9d6b8b69ff6f59a0083b8b27ac0f39246381595c
Yaroslav.Yasynskyy <Yaroslav.Yasynskyy@NTBK-9.webcanada.local>
2016-11-04 13:23:23 -0400

    Removed non Intelier projects

--- tutorial1/src/test/java/net/serenitybdd/tutorials/ui/CurrentPage.java
+++
@@ -1,9 +0,0 @@
-package net.serenitybdd.tutorials.ui;
-
-import net.serenitybdd.core.pages.PageObject;
-
-/**
- * Created by john on 10/05/2016.
- */
-public class CurrentPage extends PageObject {
-}
